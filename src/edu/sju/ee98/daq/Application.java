cf6219dde97bf4e9bdb92e1d5432a8e570d1bb31
Leo <Leo@LeoHsueh-NB>
2013-06-10 11:40:05 +0800

    Main Application.

---
+++ src/edu/sju/ee98/daq/Application.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.sju.ee98.daq;
+
+import edu.sju.ee98.daq.ui.MainFrame;
+
+/**
+ *
+ * @author Leo
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+}
